27d0e0eeed13508385c16404a101531d2661167c
16001538 <ahmedheshamouf2022@gmail.com>
2026-05-20 03:19:46 +0300

    fix: redirect password reset link to React frontend port 5173

--- controllers/authController.js
+++ controllers/authController.js
@@ -129,7 +129,7 @@ exports.forgotPassword = async (req, res, next) => {
 
     await user.save();
 
-    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/auth/reset-password/${resetToken}`;
+    const resetUrl = `http://localhost:5173/reset-password/${resetToken}`;
 
     try {
       await sendResetEmail(user.email, resetUrl);
