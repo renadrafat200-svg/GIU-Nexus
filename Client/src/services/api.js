0c29ec2294c58549a61f0e8d9c2976c9f78c8045
Judy Elrayess <elrayessjudy@gmail.com>
2026-09-11 22:57:55 +0300

    Use environment variable for API base URL

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api/v1',
+ baseURL: process.env.REACT_APP_API_URL || '/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
